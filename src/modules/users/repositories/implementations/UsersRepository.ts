9e24d3cfc5e61625c46032cd3f7f2664ff5af691
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-07-08 12:05:23 -0300

    Implementando o repositório do Usuário

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,42 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => {
+      return user.id === id;
+    });
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => {
+      return user.email === email;
+    });
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userInRepository = this.findById(receivedUser.id);
+    if (userInRepository) {
+      userInRepository.admin = true;
+      return userInRepository;
+    }
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
